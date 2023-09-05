3a198df6c984eb71141d0b6c9afa17777b39d5cd
huynhduy2078 <52554839+huynhduy2078@users.noreply.github.com>
2023-09-05 17:15:30 +0700

    fix 181

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -388,7 +388,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
     private func checkUnlockedAndFulfill(action: CXAnswerCallAction, counter: Int) {
         if UIApplication.shared.isProtectedDataAvailable {
             sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ACCEPT, self.data?.toJSON())
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(5000)) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(3000)) {
                 action.fulfill()
             }
         } else if counter > 30 { // fail if waiting for more then 3 minutes
